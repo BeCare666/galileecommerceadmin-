7b617777db3532d4954e8f1b94367441aacc2022
BeCare666 <becare.fr.ge@gmail.com>
2025-09-01 15:13:57 +0100

    cv

--- src/components/countrySelect/countrySelect.tsx
+++ src/components/countrySelect/countrySelect.tsx
@@ -18,7 +18,11 @@ const CountrySelect: React.FC<CountrySelectProps> = ({ value, onChange }) => {
     useEffect(() => {
         async function fetchCountries() {
             try {
-                const res = await fetch('http://127.0.0.1:5000/api/countries');
+                const API_URL = process.env.NEXT_PUBLIC_REST_API_ENDPOINT;
+                if (!API_URL) {
+                    throw new Error("NEXT_PUBLIC_REST_API_ENDPOINT n'est pas défini !");
+                }
+                const res = await fetch(`${API_URL}/countries`);
                 const data = await res.json();
                 setCountries(data);
             } catch (error) {
