d9b17b28e81f595487d5140f2eb57ecb1be615b5
BeCare666 <becare.fr.ge@gmail.com>
2025-09-14 13:09:24 +0100

    tg

--- next.config.js
+++ next.config.js
@@ -11,7 +11,10 @@ const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
   reactStrictMode: true,
-  i18n,
+  i18n: {
+    locales: ['en', 'fr', 'ar'], // langues supportées
+    defaultLocale: 'fr',         // langue par défaut
+  },
   images: {
     domains: [
       '127.0.0.1:8000',
